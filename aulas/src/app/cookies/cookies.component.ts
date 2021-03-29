ddfc3d7c7bec7fd0b2f695e93140d395e28434ec
Filipa0502 <filipadgsillva@gmail.com>
2021-03-29 12:51:37 +0100

    cookies

--- aulas/src/app/cookies/cookies.component.ts
+++ aulas/src/app/cookies/cookies.component.ts
@@ -10,6 +10,22 @@ export class CookiesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    /*this.getCookies();*/
   }
 
+  aceiteiCookie : boolean;
+
+  aceitarCookies(cookie : HTMLElement)
+  {
+    localStorage.setItem("cookie", "1");
+    cookie.style.display = "none";
+  }
+  lerCookies()
+  {
+    /*
+      Operador ternario
+      ? = se; : = senao
+    */
+    this.aceiteiCookie = localStorage.getItem("cookie") == "1" ? true : false;
+  }
 }
